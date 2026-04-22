2af18cdbe53cbc557d73524c4c97eded6f13fd7d
BLOOD212 <g95829914@gmail.com>
2026-04-22 23:43:09 +0200

    Update set-welcomr.js

--- plugins/set-welcomr.js
+++ plugins/set-welcomr.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply(`✅ **Messaggio impostato!**\nOgni volta che qualcuno entrerà in **questo** gruppo, vedrà questo testo.`)
+    m.reply(`✅ *Messaggio impostato!*\nOgni volta che qualcuno entrerà in *
+*questo* gruppo, vedrà questo testo.`)
   } else {
-    m.reply(`❓ **Guida Benvenuto**\nUsa: \`${usedPrefix + command} <testo>\`\n\n*Tag disponibili:*\n- **@user** (Menziona l'utente)\n- **@group** (Nome gruppo)\n\n*Esempio:* \`${usedPrefix + command} Ciao @user benvenuto in @group!\``)
+    m.reply(`❓ *Guida Benvenuto*\nUsa: \`${usedPrefix + command} <testo>\`\n\n*Tag disponibili:*\n- *@user* (Menziona l'utente)\n- *@group** (Nome gruppo)\n\n*Esempio:* \`${usedPrefix + command} Ciao @user benvenuto in @group!\``)
   }
 }
 handler.command = ['setwelcome']
